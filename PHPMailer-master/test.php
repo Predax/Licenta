e205ef551a20c40ed9335123cd466bcef3b9dadf
Preda Ionut-Iulian <preda.ionut.iulian@gmail.com>
2017-07-04 23:58:27 +0300

    license

--- PHPMailer-master/test.php
+++ PHPMailer-master/test.php
@@ -1,4 +1,7 @@
 <?php
+/* 
+This class for sending mail is adapted. Is also specified in the refferences of the licence. 
+*/
 //include 'OwnFunctions'; 
 require 'PHPMailerAutoload.php';
 
@@ -14,8 +17,8 @@ $mail->Password = $defaultPass;                          // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 25;                                    // TCP port to connect to
 
-$mail->setFrom('preda.ionut.iulian@gmail.com', 'ReqMe');
-$mail->addAddress('cosminaloredana20@gmail.com');     // Add a recipient
+$mail->setFrom($_SESSION["email"], 'ReqMe'); 
+$mail->addAddress('preda.ionut.iulian@gmail.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
